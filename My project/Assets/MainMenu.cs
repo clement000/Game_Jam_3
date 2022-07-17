c143b073353bc78eef3484a569c5e9021edb354f
Eloi <e.dussy-lachaud@wanadoo.fr>
2022-07-17 16:00:49 +0200

    Added game over menu

--- My project/Assets/MainMenu.cs
+++ My project/Assets/MainMenu.cs
@@ -10,6 +10,12 @@ public class MainMenu : MonoBehaviour
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 	}
 
+	public void PlayAgain()
+	{
+	SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+
+	}
+
 	public void QuitGame () 
 	{
 		Debug.Log("Quit!");
